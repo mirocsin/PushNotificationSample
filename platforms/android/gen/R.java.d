dcf0cd5d795ac3dedef7759849b9e0d2bc13cb0a
mirocsin <mike@qbit.com.sg>
2013-12-11 16:40:18 +0800

    a

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+c:\Temp\phonegap\PushNotificationSample\platforms\android\gen\com\pushapp\R.java \
+ : c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable\icon.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-hdpi\icon.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-hdpi\ic_launcher.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-ldpi\icon.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-ldpi\ic_launcher.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-mdpi\icon.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-mdpi\ic_launcher.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-xhdpi\icon.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\layout\main.xml \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\values\strings.xml \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\res\xml\config.xml \
+c:\Temp\phonegap\PushNotificationSample\platforms\android\bin\AndroidManifest.xml \
